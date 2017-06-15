590076dd02b3394563fe23af30cc9adf736eb1bb
Tomonori Izumida <tmizmd@gmail.com>
2017-06-15 21:29:03 +0900

    small change

--- graph/basic.lean
+++ graph/basic.lean
@@ -16,5 +16,16 @@ end graph
 
 def fin_graph {n} : (fin n → fin n → ℕ) → graph := λ spec, ⟨fin n, λ i j, fin (spec i j)⟩ 
 
+structure rooted_graph extends graph := 
+  (root : vertex)
+  (root_reachable : Π (v : vertex), graph.path root v)
+  (root_init  : graph.incoming root → false)
 
  
+namespace rooted_graph 
+variable {g : rooted_graph}
+#check (λ w : g.vertex, root_reachable g w) 
+def dom v (w : g.vertex) : Prop := ∀ (p : g.root_reachable w), v ∈ p.occurrences
+
+
+end rooted_graph
